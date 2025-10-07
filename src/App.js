b37028986c79af8c483221301573ff38f59902f3
AmanQureshi0111 <amanq7362@gmail.com>
2025-10-07 16:21:52 +0530

    start on host

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () =>{
     return(
-        <diV>
+        <div>
             App
-        </diV>
+        </div>
     )
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Note the '/client' for React 18+
+import App from './App'; // Make sure App.js exists in the same directory
 
-const root=ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  <React.StrictMode>
     <App />
-)
+  </React.StrictMode>
+);
