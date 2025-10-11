b197f353b2d9aebf1c3d4a95a4816f307a2577bd
AmanQureshi0111 <amanq7362@gmail.com>
2025-10-11 22:07:51 +0530

    scroll bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,21 @@
-import React,{useState} from 'react'
-import {Box} from '@mui/material'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
-import HeroBanner from '../components/HeroBanner'
-import SearchExercise from '../components/SearchExercise'
-import Exercises from '../components/Exercises'
-const Home= () =>{
-    return (
-        <Box>
-            <HeroBanner />
-            <SearchExercise />
-            <Exercises />
-        </Box>
-    )
-}
-export default Home
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
+  );
+};
+
+export default Home;
